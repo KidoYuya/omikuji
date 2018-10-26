3f58fbfa168f9d4a69aa6e7989ce7d737073edf3
KidoYuya <prideofisho92@gmail.com>
2018-10-26 16:29:49 +0900

    main.js ランダム機能追加

--- js/main.js
+++ js/main.js
@@ -3,13 +3,15 @@
 
   var btn = document.getElementById('btn');
 
-  btn.addEventListener('click', function(){
-    this.textContent = 'hit';
+  btn.addEventListener('click',function() {
+    var results = ['大吉','中吉','凶'];
+    var n = Math.floor(Math.random() * results.length);
+    this.textContent = results[n];
   });
-  btn.addEventListener('mousedown', function(){
+  btn.addEventListener('mousedown',function() {
     this.className = 'pushed';
   });
-  btn.addEventListener('mouseup', function(){
-    this.className = 'pushed';
+  btn.addEventListener('mouseup',function() {
+    this.className = '';
   });
 })();
